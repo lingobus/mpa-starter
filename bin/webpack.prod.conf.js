3a466273164777461f8facc2a78fddee04ce4217
zhangdaiyan <zhangdaiyan@vipkid.com.cn>
2018-10-18 15:05:14 +0800

    split node_modules into serperated vender.[contenthash].js

--- bin/webpack.prod.conf.js
+++ bin/webpack.prod.conf.js
@@ -23,39 +23,55 @@ const stylusLoader = {
   ]
 }
 
-var prodConf = {
-  mode: 'production',
-  module: {
-    rules: [
-      stylusLoader,
-      cssLoader,
-    ]
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: '[name].[contenthash].css'
-    }),
-    new webpack.optimize.OccurrenceOrderPlugin(),
-  ],
-  optimization: {
-    // https://github.com/webpack-contrib/mini-css-extract-plugin/tree/v0.4.4#minimizing-for-production
-    minimizer: [
-      new UglifyJsPlugin({
-        cache: true,
-        parallel: true,
-        sourceMap: true // set to true if you want JS source maps
-      }),
-      new OptimizeCSSAssetsPlugin({})
-    ]
-  },
-}
-
-if (process.env.HOSTALIAS) {
-  console.log('HOSTALIAS=', process.env.HOSTALIAS)
-  prodConf.devtool = '#eval-source-map'
-}
-
 function getProdConf (params) {
+  const prodConf = {
+    mode: 'production',
+    module: {
+      rules: [
+        stylusLoader,
+        cssLoader,
+      ]
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: '[name].[contenthash].css'
+      }),
+      new webpack.optimize.OccurrenceOrderPlugin(),
+    ],
+    optimization: {
+      // https://github.com/webpack-contrib/mini-css-extract-plugin/tree/v0.4.4#minimizing-for-production
+      minimizer: [
+        new UglifyJsPlugin({
+          cache: true,
+          parallel: true,
+          sourceMap: true // set to true if you want JS source maps
+        }),
+        new OptimizeCSSAssetsPlugin({})
+      ],
+      // https://webpack.js.org/configuration/optimization/#optimization-runtimechunk
+      runtimeChunk: {
+        name: 'pages/' + params.name + '/runtime',
+      },
+      // https://webpack.js.org/plugins/split-chunks-plugin/#optimization-splitchunks
+      splitChunks: {
+        minSize: 30000, // Minimum size, in bytes, for a chunk to be generated. default 30000
+        // split node_modules into serperated vender.[contenthash].js
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'pages/' + params.name + '/venders',
+            chunks: 'all'
+          }
+        }
+      }
+    },
+  }
+
+  if (process.env.HOSTALIAS) {
+    console.log('HOSTALIAS=', process.env.HOSTALIAS)
+    prodConf.devtool = '#eval-source-map'
+  }
+
   return merge(getBaseConf(params), prodConf)
 }
 
